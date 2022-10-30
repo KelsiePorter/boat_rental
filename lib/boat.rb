b531110371573a8822c4fd4b6b6cc203f04da163
Kelsie Porter <krporter22@gmail.com>
2022-10-30 15:56:52 -0600

    Create Boat class and initial attributes

---
+++ lib/boat.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+class Boat 
+  attr_reader :type,
+              :price_per_hour,
+              :hours_rented
+
+  def initialize(type, price_per_hour)
+    @type = type 
+    @price_per_hour = price_per_hour
+    @hours_rented = 0
+  end
+
+end
