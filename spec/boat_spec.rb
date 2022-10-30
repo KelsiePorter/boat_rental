096022c38cd2488d2ad61f89a1c0d6ffa8bd7e75
Kelsie Porter <krporter22@gmail.com>
2022-10-30 15:57:30 -0600

    Create initial Boat class tests

---
+++ spec/boat_spec.rb
@@ -0,0 +1,25 @@
+require 'pry'
+require './lib/boat'
+require './lib/renter'
+
+RSpec.describe Boat do
+  it 'boat has a type and a price_per_hour' do 
+    kayak = Boat.new(:kayak, 20) 
+
+    expect(kayak.type).to eq(:kayak)
+    expect(kayak.price_per_hour).to eq(20)
+  end
+
+  it 'boat is rented for 0 hours by default' do 
+    kayak = Boat.new(:kayak, 20) 
+
+    expect(kayak.hours_rented).to eq(0)
+  end
+
+  it 'boat can be rented by the hour' do 
+    kayak = Boat.new(:kayak, 20) 
+
+    
+  end
+end
+  
