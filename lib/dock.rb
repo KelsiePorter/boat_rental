acb478bb5d2f92d9c9693ab6b2b058904413bdfc
Kelsie Porter <krporter22@gmail.com>
2022-10-30 20:01:13 -0600

    Create #charge and #hours_rented helper

--- lib/dock.rb
+++ lib/dock.rb
@@ -15,4 +15,30 @@ class Dock
     @rental_log[boat] = renter
   end
 
+  def charge(boat)
+    payment = {}
+    payment[:card_number] = @rental_log[boat].credit_card_number
+    hours_charged = hours_rented(boat) * boat.price_per_hour
+    payment[:amount] = hours_charged
+    payment
+  end
+
+  private
+
+  def hours_rented(boat) 
+    # option #1
+    # hours_rented = boat.hours_rented
+    # if hours_rented > @max_rental_time
+    #   hours_rented = @max_rental_time
+    # end
+    # option 2
+    # if boat.hours_rented > @max_rental_time
+    #   @max_rental_time
+    # else 
+    #   boat.hours_rented
+    # end
+    # option 3
+    [boat.hours_rented, @max_rental_time].min
+  end
+
 end
